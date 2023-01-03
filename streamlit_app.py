35e419524f11a854d227feb9ab27837e83bd38ed
padma2500 <121706652+padma2500@users.noreply.github.com>
2023-01-03 11:38:13 +0530

    Update streamlit_app.py
    indent fix 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,8 +36,8 @@ try:
       streamlit.dataframe(fruityvice_normalized)
       
  except URLError as e:
-   streamlit.error()
-      
+     streamlit.error()
+   
 #streamlit.write('The user entered ', fruit_choice)
 
 #import requests
