7cec4a569cf2649488d5c76547347d778eabc86c
padma2500 <121706652+padma2500@users.noreply.github.com>
2023-01-03 11:15:00 +0530

    Update streamlit_app.py
    control flow testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,3 +60,5 @@ streamlit.dataframe(my_data_row)
 
 fruit_added = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('Thanks for adding ', fruit_added)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
